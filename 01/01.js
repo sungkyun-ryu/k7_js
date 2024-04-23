de97b3a46d77f1aed0e576bc11d1ce9acab70a25
sungkyun-ryu <sungkyun.ryu@gmail.com>
2024-04-23 17:46:10 +0900

    Button Practice

---
+++ 01/01.js
@@ -0,0 +1,11 @@
+function handleClick(n) {
+        // document.querySelector('#msg').innerHTML = '<h1>Hello!</h1>'
+        document.querySelector('#msg').innerHTML = `<h1>Hello! ${n}</h1>`; 
+ }
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    // document.getElementById('msg').innerHTML = "Hello!" ;
+    // document.querySelector('#msg').innerHTML = '<h1>Hello!</h1>'
+
+});
